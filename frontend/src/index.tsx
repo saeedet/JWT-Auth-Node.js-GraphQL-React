610c470da0937ce33990875fa20ca28023abd555
Saeed <saeed_et90@yahoo.com>
2021-12-06 13:12:47 +1100

    persisting user session apollo-link

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,10 +8,57 @@ import {
   HttpLink,
   InMemoryCache,
 } from "@apollo/client";
-import { getAccessToken } from "./accessToken";
+import { getAccessToken, setAccessToken } from "./accessToken";
 import AuthMiddleware from "./AuthMiddleware";
+import { onError } from "@apollo/client/link/error";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import jwtDecode, { JwtPayload } from "jwt-decode";
+import axios from "axios";
 
-const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
+const tokenRefreshLink = new TokenRefreshLink({
+  accessTokenField: "accessToken",
+  isTokenValidOrUndefined: () => {
+    const token = getAccessToken();
+
+    if (!token) {
+      return false;
+    }
+    try {
+      const { exp } = jwtDecode<JwtPayload>(token);
+      if (Date.now() >= exp! * 1000) {
+        return false;
+      } else {
+        return true;
+      }
+    } catch {
+      return false;
+    }
+  },
+  fetchAccessToken: () => {
+    return axios.get("http://localhost:4000/refresh_token", {
+      withCredentials: true,
+    });
+  },
+  handleFetch: (accessToken) => {
+    setAccessToken(accessToken);
+  },
+  handleError: (err) => {
+    // full control over handling token fetch Error
+    console.warn("Your refresh token is invalid. Try to relogin");
+    console.error(err);
+  },
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 const authLink = new ApolloLink((operation, forward) => {
   const token = getAccessToken();
@@ -28,9 +75,16 @@ const authLink = new ApolloLink((operation, forward) => {
 });
 
 const client = new ApolloClient({
+  link: ApolloLink.from([
+    tokenRefreshLink,
+    errorLink,
+    authLink,
+    new HttpLink({
+      uri: "http://localhost:4000/graphql",
+      credentials: "include",
+    }),
+  ]),
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
-  credentials: "include",
 });
 
 ReactDOM.render(
